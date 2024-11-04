ac95365a7d9e90853c202cf94576e3f83f13cf8c
ikari <sydnazar08@gmail.com>
2024-11-04 20:25:10 +0200

    add function for adding book

--- Project_Library.cpp
+++ Project_Library.cpp
@@ -9,5 +9,38 @@ struct Book { //структура до книг
     int year;
     string genre;
 };
+
+void addBook(Book*& books, int& bookCount) {
+    Book newBook;
+    cout << "Enter book ID: ";
+    cin >> newBook.id;
+
+    // перевірка на наявність книги з таким айді 
+    for (int i = 0; i < bookCount; i++) {
+        if (books[i].id == newBook.id) {
+            cout << "Error: Book with this ID already exists.\n";
+            return;  // виводим якщо книга є.
+        }
+    }
+    cout << "Enter title (one word): ";
+    cin >> newBook.title;
+    cout << "Enter author (one word): ";
+    cin >> newBook.author;
+    cout << "Enter publication year: ";
+    cin >> newBook.year;
+    cout << "Enter genre (one word): ";
+    cin >> newBook.genre;
+    Book* temp = new Book[bookCount + 1];
+    for (int i = 0; i < bookCount; i++) { // додавання новоъ книги
+        temp[i] = books[i];
+    }
+    temp[bookCount] = newBook;
+    delete[] books;
+    books = temp;
+    bookCount++;
+    cout << "Book added successfully!" << endl;
+
 int main(){
+    Book* books = 0;// динамічний масив для зберігання книг та його розмір
+    int bookCount = 0;
 }
